300d6097eeeb8d33a77f4c0314721854b648e6b3
Abeer Ali Khan <aalkhan@students.uit.edu>
2023-03-26 23:32:51 +0500

    complete api

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+const PORT = 8000;
+
+const getData = async (filepath) => {
+  const data = await fs.promises.readFile(filepath, "utf8");
+
+  return JSON.parse(data);
+};
+
+// Get all categories
+app.get("api/categories", async (req, res) => {
+  const categories = await getData("./categories.json");
+  res.json(categories);
+});
+
+// Get all products for that category
+app.get("api/categories/:categoryId", async (req, res) => {
+  const categoryId = +req.params.categoryId;
+  let products = await getData("./products.json");
+  products = products.filter((product) => product.categoryId === categoryId);
+  if (!products.length) return res.json({ error: "Category does not exist!" });
+
+  res.json(products);
+});
+
+// Get a single product
+app.get("api/products/:productId", async (req, res) => {
+  const productId = +req.params.productId;
+  const products = await getData("./products.json");
+  const product = products.find((product) => product.id === productId);
+
+  if (!product) return res.json({ error: "Product does not exist!" });
+
+  res.json(product);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listen at port ${PORT}`);
+});
